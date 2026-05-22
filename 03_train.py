055c776e3ddcfc3cec7ebf7e8e1c60725dcef460
JALAJ MISHRA <mjalaj77@gmail.com>
2026-05-22 14:12:08 +0530

    Training script is being written

---
+++ 03_train.py
@@ -0,0 +1,83 @@
+
+# Imports
+import torch
+import torch.nn as nn
+from torch.optim import AdamW
+from torch.utils.data import Dataset, DataLoader
+from transformers import BertForSequenceClassification, BertTokenizer, get_linear_schedule_with_warmup
+from datasets import load_dataset
+from sklearn.metrics import accuracy_score, classification_report
+import numpy as np
+
+
+# CONFIG
+MODEL_NAME='bert-base-uncased'
+MAX_LENGTH='128'
+BATCH_SIZE='16'
+EPOCHS='3'
+LR='2e-5'
+TRAIN_SIZE=2000
+TEST_SIZE=500
+SAVE_PATH='./models/bert_sentiment.pth'
+DEVICE=torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+# DEVICE
+print(f'Using device: {DEVICE}')
+
+
+
+# DATASET
+class IMDBDataset(Dataset):
+    def __init__(self, data, tokenizer):
+        self.texts = [d['texts'] for d in data]
+        self.labels = [d['labels'] for d in data]
+        self.tokenizer = tokenizer
+    
+    def __len__(self):
+        return len(self.labels)
+
+    def __getitem__(self, index):
+        enc = self.tokeinzer(self.texts[index], 
+            padding='max_lenght', 
+            truncation=True, 
+            max_length=MAX_LENGTH, 
+            return_tensors='pt'
+        )
+
+        return {
+            'input_ids': enc['input_ids'].squeeze(0), 
+            'attention_mask':enc['attention_mask'].squeeze(0), 
+            'label': torch.tensor(self.labels[index], dtype=torch.long)} 
+
+
+
+# LOAD DATASET
+print('Loading dataset...')
+
+dataset = load_dataset('imdb')
+train_data = dataset['train'].select(range(TRAIN_SIZE))
+test_data = dataset['test'].select(range(TEST_SIZE))
+tokenizer = BertTokenizer.from_pretrained(MODEL_NAME)
+
+train_loader = DataLoader(IMDBDataset(train_data, tokenizer), batch_size=BATCH_SIZE, shuffle=True)
+test_loader = DataLoader(IMDBDataset(test_data, tokenizer), batch_size=BATCH_SIZE)
+
+print(f'Train: {TRAIN_SIZE} | Test: {TEST_SIZE} | Batches/epoch: {len(train_loader)}')
+
+
+
+# MODEL
+print('Loading BERT model...')
+
+model = BertForSequenceClassification.from_pretrained(MODEL_NAME, num_labels=2)
+model.to(DEVICE)
+
+
+
+# OPTIMIZER
+optimizer = AdamW(model.parameters(), lr=LR, weight_decay=0.01)
+total_steps = len(train_loader) * EPOCHS
+scheduler = get_linear_schedule_with_warmup(optimizer, num_warmup_steps=total_steps//10, num_training_steps=total_steps)
+
+
+# TRAINING LOOP
